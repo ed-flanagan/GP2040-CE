f14cc471f574d13da44100bf146d59323c0c26f9
Ed Flanagan <ed@flanagan.xyz>
2025-12-18 00:49:39 -0800

    wip: tweaking i2c

--- configs/Haute42COSMOXC16S/BoardConfig.h
+++ configs/Haute42COSMOXC16S/BoardConfig.h
@@ -120,7 +120,7 @@
 // clang-format off
 #define DEFAULT_BOARD_LAYOUT_A                                                \
 {                                                                             \
-    {GP_ELEMENT_PIN_BUTTON, {20, 32, 20, 24, 1, 1, 18, GP_SHAPE_PILL, 112}},  \
+    {GP_ELEMENT_PIN_BUTTON, {20, 32, 20, 22, 1, 1, 18, GP_SHAPE_PILL, 112}},  \
     {GP_ELEMENT_DIR_BUTTON, {32, 23, 4,  4,  1, 1, 5,  GP_SHAPE_ELLIPSE}},    \
     {GP_ELEMENT_DIR_BUTTON, {43, 23, 4,  4,  1, 1, 3,  GP_SHAPE_ELLIPSE}},    \
     {GP_ELEMENT_DIR_BUTTON, {52, 28, 4,  4,  1, 1, 4,  GP_SHAPE_ELLIPSE}},    \
@@ -137,7 +137,7 @@
     {GP_ELEMENT_PIN_BUTTON, {74,  35, 4,   4,  1, 1, 6,  GP_SHAPE_ELLIPSE}},  \
     {GP_ELEMENT_PIN_BUTTON, {83,  30, 4,   4,  1, 1, 7,  GP_SHAPE_ELLIPSE}},  \
     {GP_ELEMENT_PIN_BUTTON, {94,  30, 4,   4,  1, 1, 8,  GP_SHAPE_ELLIPSE}},  \
-    {GP_ELEMENT_PIN_BUTTON, {106, 32, 106, 24, 1, 1, 19, GP_SHAPE_PILL, 68}}, \
+    {GP_ELEMENT_PIN_BUTTON, {106, 32, 106, 22, 1, 1, 19, GP_SHAPE_PILL, 68}}, \
     {GP_ELEMENT_PIN_BUTTON, {71,  53, 71,  47, 1, 1, 27, GP_SHAPE_PILL, 45}}, \
     {GP_ELEMENT_PIN_BUTTON, {79,  44, 4,   4,  1, 1, 9,  GP_SHAPE_ELLIPSE}},  \
 }
